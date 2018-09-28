dcced7f4df1b0efdcfd61212a34b66f9bcc48fec
Kayler C <kmcookhorn@gmail.com>
2018-09-28 11:58:21 -0400

    login setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+root 'pages#home'
+
+get 'register' => 'users#new'
+  # post 'register' => 'users#show'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  resources :users
+
+  get 'pages/about'
+  get 'pages/help'
+
+  # get 'pages/home'
+
+  # get 'users/new'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/edit'
+
+  # get 'sessions/new'
+  # get 'sessions/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
